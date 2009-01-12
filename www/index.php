1f79769416dfe22890055c50e198594a7ba67b98
Jason Hutchens <JasonHutchens@gmail.com>
2009-01-12 14:22:02 +0900

    Basic layout, copied from mh10

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<?
+require_once("lib/l10n.php");
+require_once("lib/gravatar.php");
+print '<?xml version="1.0" encoding="utf-8"?>'."\n";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head> 
+<title>shelvr</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+</head>
+<body>
+<h1><?echo _("Hello World");?></h1>
+<?
+$email = "JasonHutchens@gmail.com";
+$default = "http://members.scottsdaleculinaryfestival.org/portals/18/default_avatar.jpg"; 
+$gravatar = new Gravatar($email, $default);
+$gravatar->size = 128;
+$gravatar->rating = "G";
+echo $gravatar;
+?>
+</body>
+</html>
---
+++ www/lib/l10n.php
@@ -0,0 +1,18 @@
+<?require_once("locale/supported.php");?>
+<?
+$arr=explode(",", str_replace(";", ",", $_SERVER['HTTP_ACCEPT_LANGUAGE']));
+foreach($arr as $val)
+{
+    if($val[1]=='=') continue;
+    $language = str_replace("-", "_", $val);
+    if(array_key_exists($language, $supported))
+    {
+        putenv("LANGUAGE=$language");
+        break;
+    }
+}
+$domain = 'messages';
+bindtextdomain($domain, "./locale");
+bind_textdomain_codeset($domain, "UTF-8");
+textdomain($domain);
+?>
