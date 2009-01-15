f68a2d7c8bfe83903ff97a5f3d15e3f28aaa361f
U-Gray\Jack <Jack@Gray.(none)>
2009-01-15 16:21:57 +0900

    added amazon test deploy

--- update.rb
+++ update.rb
@@ -3,5 +3,7 @@ require 'fileutils'
 FileUtils.rm_rf('c:\wamp\www\shelvr')
 FileUtils.rm_rf('c:\wamp\www\fanglr')
 FileUtils.rm_rf('c:\wamp\www\demo')
+FileUtils.rm_rf('c:\wamp\www\amazontest')
 FileUtils.cp_r('www', 'c:\wamp\www\fanglr', :preserve => true)
 FileUtils.cp_r('demo', 'c:\wamp\www\demo', :preserve => true)
+FileUtils.cp_r('amazontest', 'c:\wamp\www\amazontest', :preserve => true)
