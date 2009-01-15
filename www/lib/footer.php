3d84982d177436660ddd017878b2a387d643d593
Jason Hutchens <JasonHutchens@gmail.com>
2009-01-15 15:56:04 +0900

    Added google analytics

--- www/lib/footer.php
+++ www/lib/footer.php
@@ -13,5 +13,14 @@
   RPXNOW.realm = "fanglr";
   RPXNOW.overlay = true;
 </script>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-7021515-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 </html>
